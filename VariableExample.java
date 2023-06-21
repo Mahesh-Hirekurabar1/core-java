57ab9e93f8425a251b49a63d963ddce87dd72418
Mahesh-Hirekurabar1 <137083341+Mahesh-Hirekurabar1@users.noreply.github.com>
2023-06-21 12:30:02 +0530

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,95 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        // long
+        long population = 10000000000L;
+        long distance = 500000L;
+        long timestamp = 1624117152000L;
+        long revenue = 5000000000L;
+        long fileSize = 2500000000L;
+
+        // short
+        short temp = 25;
+        short quantity = 100;
+        short speed = 50;
+        short score = 80;
+        short rank = 5;
+
+        // boolean
+        boolean isRaining = true;
+        boolean isSunny = false;
+        boolean hasPassedExam = true;
+        boolean isWorking = false;
+        boolean isTall = true;
+
+        // int
+        int age = 30;
+        int numberOfStudents = 50;
+        int timeInSeconds = 120;
+        int pagesInBook = 300;
+        int salary = 5000;
+
+        // char
+        char grade = 'A';
+        char symbol = '#';
+        char gender = 'M';
+        char initial = 'J';
+        char currency = '$';
+
+        // double
+        double temp = 25.5;
+        double price = 9.99;
+        double height = 1.85;
+        double weight = 75.5;
+        double GPA = 3.75;
+
+        // byte
+        byte age = 30;
+        byte quantity = 100;
+        byte speed = 50;
+        byte score = 80;
+        byte rank = 5;
+
+        // Printing meaningful sentences using the variables
+        System.out.println("The population of the city is " + population);
+        System.out.println("The distance traveled is " + distance + " meters");
+        System.out.println("The timestamp is " + timestamp);
+        System.out.println("The revenue generated is $" + revenue);
+        System.out.println("The file size is " + fileSize + " bytes");
+
+        System.out.println("The temperature outside is " + temperature + " degrees Celsius");
+        System.out.println("The quantity available is " + quantity);
+        System.out.println("The speed of the vehicle is " + speed + " km/h");
+        System.out.println("The score in the game is " + score);
+        System.out.println("The rank achieved is " + rank);
+
+        System.out.println("Is it raining? " + isRaining);
+        System.out.println("Is it sunny? " + isSunny);
+        System.out.println("Has the person passed the exam? " + hasPassedExam);
+        System.out.println("Is the person working? " + isWorking);
+        System.out.println("Is the person tall? " + isTall);
+
+        System.out.println("The age of the person is " + age);
+        System.out.println("The number of students in the class is " + numberOfStudents);
+        System.out.println("The time in seconds is " + timeInSeconds);
+        System.out.println("The book has " + pagesInBook + " pages");
+        System.out.println("The salary is $" + salary);
+
+        System.out.println("The grade obtained in the exam is " + grade);
+        System.out.println("The symbol used is " + symbol);
+        System.out.println("The gender of the person is " + gender);
+        System.out.println("The initial of the name is " + initial);
+        System.out.println("The currency symbol is " + currency);
+
+        System.out.println("The temperature outside is " + temperature + " degrees Celsius");
+        System.out.println("The price of the product is $" + price);
+        System.out.println("The height of the person is " + height + " meters");
+        System.out.println("The weight of the person is " + weight + " kilograms");
+        System.out.println("The GPA achieved in the semester is " + GPA);
+
+        System.out.println("The age of the person is " + age);
+        System.out.println("The quantity available is " + quantity);
+        System.out.println("The speed of the vehicle is " + speed + " km/h");
+        System.out.println("The score in the game is " + score);
+        System.out.println("The rank achieved is " + rank);
+    }
+}
