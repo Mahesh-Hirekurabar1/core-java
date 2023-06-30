5b5a6a5b81acf6d35bfb62bab784ff22cf1a12dc
Mahesh-Hirekurabar1 <137083341+Mahesh-Hirekurabar1@users.noreply.github.com>
2023-06-30 08:30:25 +0530

    Add files via upload

---
+++ GivenTask.java
@@ -0,0 +1,83 @@
+//Home work 1
+public class GivenTask{
+
+public static void main(String[] args){
+	int NumberOfClasses = 3;
+	int TeachingHours = 6;
+	int NumberOfStudents = 9;
+	int NumberOfRooms = 10;
+	int TravalingCharge = 20;
+	int MobileCost = 6000;
+	int NumberOfPages = 500;
+	int NumberOfBooks = 45;
+	int NumberOfStudentsInPg =410;
+	int NumberOfInstitutions = 20;
+	String NameOfInstitute ="X-workz";
+	String NameOfStudent ="Mahesh";
+	String NameOfBookAuthor ="N.K.Shah";
+	String BookName ="C Programming";
+	String PgName ="Venkatesh";
+	String LaptopCompany = "Asus";
+	String BagBrand = "Asus";
+	String MobileCompany = "Realme";
+	String CityName ="Bengalore";
+	String StreatName = "jayanagar Layout";
+	
+
+	System.out.println(NumberOfClasses);
+	System.out.println(TeachingHours);
+	System.out.println(NumberOfStudents);
+	System.out.println(NumberOfRooms);
+	System.out.println(TravalingCharge);
+	System.out.println(MobileCost);
+	System.out.println(NumberOfPages);
+	System.out.println(NumberOfBooks);
+	System.out.println(NumberOfStudentsInPg);
+	System.out.println(NumberOfInstitutions);
+
+	System.out.println(NameOfInstitute);
+	System.out.println(NameOfStudent);
+	System.out.println(NameOfBookAuthor);
+	System.out.println(BookName);
+	System.out.println(PgName);
+	System.out.println(LaptopCompany);
+	System.out.println(BagBrand);
+	System.out.println(MobileCompany);	
+	System.out.println(CityName);
+	System.out.println(StreatName);
+
+	
+
+
+	TeachingHours = 10;
+	NumberOfRooms = 20;
+	MobileCost = 3000;
+	NumberOfBooks = 56;
+	NumberOfStudents=100;
+	StreatName="PCMB";
+	CityName="Hadagali";
+
+			
+	
+	
+	System.out.println(TeachingHours);
+	System.out.println(NumberOfRooms);
+	System.out.println(MobileCost);
+	System.out.println(NumberOfBooks);
+	System.out.println(NumberOfStudents);
+	System.out.println(StreatName);
+	System.out.println(CityName);
+	System.out.println();
+	System.out.println();
+	System.out.println();
+
+	
+
+	}
+}
+	
+	
+
+	
+
+	
